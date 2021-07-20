9f2f640d3613130e30ddf078f1355c03c7eaebc7
RMYHY <60095777+RMYHY@users.noreply.github.com>
2021-07-20 17:43:57 +0800

    Update __init__.py

--- HoshinoBot/hoshino/modules/shaojo/__init__.py
+++ HoshinoBot/hoshino/modules/shaojo/__init__.py
@@ -1,19 +1,12 @@
 import random
-import asyncio
-from hoshino import Service, util, priv, config
+from hoshino import Service, util, priv
 from hoshino.typing import CQEvent
 import time
 from .choicer import Choicer
 
-forward_msg_exchange = config.FORWARD_MSG_EXCHANGE
-forward_msg_name = config.FORWARD_MSG_NAME
-forward_msg_uid = config.FORWARD_MSG_UID
-recall_msg_set = config.RECALL_MSG_SET
-RECALL_MSG_TIME = config.RECALL_MSG_TIME
-
 sv_help = '''
-- [今天我是什么少女]  查看今天的自己
-- [今天你是什么少女 @人]  查看今天的他人
+今天我要变成少女!
+今天你是什么少女
 '''.strip()
 
 sv = Service(
@@ -22,12 +15,12 @@ sv = Service(
     manage_priv = priv.ADMIN, #管理权限
     visible = True, #False隐藏
     enable_on_default = True, #是否默认启用
-    bundle = '娱乐', #属于哪一类
+    bundle = '通用', #属于哪一类
     help_ = sv_help #帮助文本
     )
 
 @sv.on_fullmatch(["帮助今天也是少女"])
-async def bangzhu_shaojo(bot, ev):
+async def bangzhu(bot, ev):
     await bot.send(ev, sv_help, at_sender=True)
 
 inst = Choicer(util.load_config(__file__))
@@ -37,36 +30,7 @@ async def my_shoujo(bot, ev: CQEvent):
     uid = ev.user_id
     name = ev.sender['card'] or ev.sender['nickname']
     msg = inst.format_msg(uid, name)
-    if forward_msg_exchange == 1:
-        data = {
-            "type": "node",
-            "data": {
-                "name": f"{forward_msg_name}",
-                "uin": f"{forward_msg_uid}",
-                "content": msg
-            }
-        }
-        if recall_msg_set == 1:
-            recall = await bot.send_group_forward_msg(group_id=ev['group_id'], messages=data)
-            notice = await bot.send(ev, f"将在{RECALL_MSG_TIME}s后将撤回消息")
-
-            await asyncio.sleep(RECALL_MSG_TIME)
-
-            await bot.delete_msg(message_id=recall['message_id'])
-            await bot.delete_msg(message_id=notice['message_id'])
-        else:
-            await bot.send_group_forward_msg(group_id=ev['group_id'], messages=data)
-    else:
-        if recall_msg_set == 1:
-            recall_1 = await bot.send(ev, msg)
-            notice = await bot.send(ev, f"将在{RECALL_MSG_TIME}s后将撤回消息")
-
-            await asyncio.sleep(RECALL_MSG_TIME)
-
-            await bot.delete_msg(message_id=recall_1['message_id'])
-            await bot.delete_msg(message_id=notice['message_id'])
-        else:
-            await bot.send(ev, msg)
+    await bot.send(ev, msg)
 
 
 @sv.on_prefix('今天你是什么少女')
@@ -85,33 +49,4 @@ async def other_shoujo(bot, ev: CQEvent):
         )
         name = info['card'] or info['nickname']
         msg = inst.format_msg(uid, name)
-        if forward_msg_exchange == 1:
-            data = {
-            "type": "node",
-            "data": {
-                "name": f"{forward_msg_name}",
-                "uin": f"{forward_msg_uid}",
-                "content": msg
-            }
-        }
-        if recall_msg_set == 1:
-            recall = await bot.send_group_forward_msg(group_id=ev['group_id'], messages=data)
-            notice = await bot.send(ev, f"将在{RECALL_MSG_TIME}s后将撤回消息")
-
-            await asyncio.sleep(RECALL_MSG_TIME)
-
-            await bot.delete_msg(message_id=recall['message_id'])
-            await bot.delete_msg(message_id=notice['message_id'])
-        else:
-            await bot.send_group_forward_msg(group_id=ev['group_id'], messages=data)
-    else:
-        if recall_msg_set == 1:
-            recall_1 = await bot.send(ev, msg)
-            notice = await bot.send(ev, f"将在{RECALL_MSG_TIME}s后将撤回消息")
-
-            await asyncio.sleep(RECALL_MSG_TIME)
-
-            await bot.delete_msg(message_id=recall_1['message_id'])
-            await bot.delete_msg(message_id=notice['message_id'])
-        else:
-            await bot.send(ev, msg)
+        await bot.send(ev, msg)
