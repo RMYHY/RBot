a2e93f1d65b2af49b7d7c99505dbed6db03d7914
RMYHY <60095777+RMYHY@users.noreply.github.com>
2021-09-27 19:02:45 -0400

    更新补偿时间计算倍率

--- HoshinoBot/hoshino/modules/hedao/hedao.py
+++ HoshinoBot/hoshino/modules/hedao/hedao.py
@@ -1,22 +1,68 @@
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@RMYHY 
+qiuyue0
+/
+pcr_timecalc
+Public
+forked from madoka315/pcr_timecalc
+0
+12
+Code
+Issues
+2
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+pcr_timecalc/pcrtimecalc.py /
+@qiuyue0
+qiuyue0 去掉了不必要的说明
+Latest commit c00dc84 8 days ago
+ History
+ 2 contributors
+@qiuyue0@madoka315
+55 lines (53 sloc)  2.16 KB
+   
 import hoshino
+import math
 from hoshino import Service, priv
 from hoshino.typing import CQEvent
 
-sv = Service('合刀', manage_priv=priv.SUPERUSER, help_='请输入：合刀 刀1伤害 刀2伤害 剩余血量\n如：合刀 50 60 70\n')
-
-
+sv = Service('合刀', manage_priv=priv.SUPERUSER, help_='请输入：合刀 刀A伤害 刀B伤害 剩余血量 [击杀刀剩余时间]\n如：合刀 500 600 700')
+class time_calc():
+    def __init__(self,dmg1:float,dmg2:float,rest:float,left_time:int):
+        self.dmg_A = min(rest,dmg1)
+        self.dmg_B = min(rest,dmg2)
+        self.rest=rest
+        self.left_time=left_time
+    def _range(self,num):
+        return round(max(min(num,90.0),21.0),2)
+    def A_first(self)->str:
+        ret = self._range(110-(self.rest-self.dmg_A)/self.dmg_B*(90-self.left_time))
+        return str(ret)
+    def B_first(self)->str:
+        ret = self._range(110-(self.rest-self.dmg_B)/self.dmg_A*(90-self.left_time))
+        return str(ret)
 @sv.on_prefix('合刀')
 async def feedback(bot, ev: CQEvent):
-    # print(ev)
-    # print(ev.raw_message)
     cmd = ev.raw_message
     content=cmd.split()
-    # print(cmd)
-    # print(cmd.split())
-    if(len(content)!=4):
-        reply="请输入：合刀 刀1伤害 刀2伤害 剩余血量\n如：合刀 50 60 70\n"
+    if(len(content) not in (4,5)):
+        reply="请输入：合刀 刀A伤害 刀B伤害 剩余血量 [击杀刀剩余时间]\n如：合刀 500 600 700"
         await bot.send(ev, reply)
         return
+    try:
+        left_time = float(content[4])
+    except:
+        left_time = 0
     d1=float(content[1])
     d2=float(content[2])
     rest=float(content[3])
@@ -24,30 +70,31 @@ async def feedback(bot, ev: CQEvent):
         reply="醒醒！这两刀是打不死boss的\n"
         await bot.send(ev, reply)
         return
-    dd1=d1
-    dd2=d2
-    if d1>=rest:
-        dd1=rest
-    if d2>=rest:
-        dd2=rest        
-    res1=(1-(rest-dd1)/dd2)*90+10; # 1先出，2能得到的时间
-    res2=(1-(rest-dd2)/dd1)*90+10; # 2先出，1能得到的时间
-    res1=round(res1,2)
-    res2=round(res2,2)
-    res1=min(res1,90)
-    res2=min(res2,90)
-    res1=str(res1)
-    res2=str(res2)
-    reply=""
+    result = time_calc(d1,d2,rest,left_time)
+    
+    reply=f"刀A伤害：{d1} 刀B伤害：{d2} BOSS血量：{rest} "
+    reply+=f"击杀刀剩余时间：{left_time}秒\n" if d1>=rest or d2>=rest else "\n"
     if(d1>=rest or d2>=rest):
-        reply=reply+"注：\n"
-        if(d1>=rest):
-            reply=reply+"第一刀可直接秒杀boss，伤害按 "+str(rest)+" 计算\n"
         if(d2>=rest):
-            reply=reply+"第二刀可直接秒杀boss，伤害按 "+str(rest)+" 计算\n"
-    d1=str(d1)
-    d2=str(d2)
-    reply=reply+d1+"先出，另一刀可获得 "+res1+" 秒补偿刀\n"
-    reply=reply+d2+"先出，另一刀可获得 "+res2+" 秒补偿刀\n"
+            reply=reply+"刀A先出，另一刀可获得 "+result.A_first()+" 秒补偿刀"
+            await bot.send(ev, reply)
+            return
+        elif(d1>=rest):
+            reply=reply+"刀B先出，另一刀可获得 "+result.B_first()+" 秒补偿刀"
+            await bot.send(ev, reply)
+            return
+    reply=reply+"刀A先出，另一刀可获得 "+result.A_first()+" 秒补偿刀\n"
+    reply=reply+"刀B先出，另一刀可获得 "+result.B_first()+" 秒补偿刀"
     await bot.send(ev, reply)
-
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
