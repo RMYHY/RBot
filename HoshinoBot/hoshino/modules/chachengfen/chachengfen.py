68da30ca69178a49807a6b30d84dab79cb49b222
RMYHY <60095777+RMYHY@users.noreply.github.com>
2021-11-08 21:57:21 -0500

    Update chachengfen.py

--- HoshinoBot/hoshino/modules/chachengfen/chachengfen.py
+++ HoshinoBot/hoshino/modules/chachengfen/chachengfen.py
@@ -56,8 +56,7 @@ async def _update_vtb_list():  # 只在启动时更新
 			hoshino.logger.error(f"更新vtb列表时发生错误:{repr(e)}")
 			return
 		data = resp.json()["vtbs"]
-		bilibili_vtbs = filter(lambda x: len(x["accounts"]) != 0 and [lambda a: a["platform"]=='bilibili',x["accounts"][0].get("platform") == 'bilibili',
-		                       data)
+		bilibili_vtbs = filter(lambda x: len(x["accounts"]) != 0 and [lambda a: a["platform"]=='bilibili',x["accounts"][0].get("platform") == 'bilibili'], data)
 		bilibili_vtbs_dict = {x["accounts"][0].get(
 			"id"): x["name"][x["name"]["default"]] for x in bilibili_vtbs}
 		with open(os.path.join(os.path.dirname(__file__), 'bilibili_vtbs.json'), 'w', encoding='utf8') as f:
